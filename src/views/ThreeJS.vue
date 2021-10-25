4dd6bba91b5a2b49ddc9fdd076b1a3c15ce2cdb8
Honza Kváš <kvasik98@seznam.cz>
2021-10-25 16:17:01 +0200

    [THREE] add light

--- src/views/ThreeJS.vue
+++ src/views/ThreeJS.vue
@@ -3,6 +3,7 @@
 
 <script>
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default {
     created () {
@@ -12,7 +13,11 @@ export default {
             material: null,
             renderer: null,
             scene: null,
-            camera: null
+            camera: null,
+            ambientLight: null,
+            pointLight: null,
+            pointLightHelper: null,
+            controls: null,
         }
     },
     methods: {
@@ -21,31 +26,49 @@ export default {
 			//---------------------------------------------------
 			this.scene = new THREE.Scene();
 			this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-			this.camera.position.z = 3;
+			this.camera.position.z = 8;
 
 			this.renderer = new THREE.WebGLRenderer();
 			this.renderer.setSize( window.innerWidth, window.innerHeight );
+            this.renderer.setPixelRatio( window.devicePixelRatio )
 			document.body.appendChild( this.renderer.domElement );
-			//---------------------------------------------------
+            //controls
+            //---------------------------------------------------
+            this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
+			//---------------------------------------------------
+            //make light
+            //---------------------------------------------------
+            this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+            this.scene.add(this.ambientLight);
+            this.pointLight = new THREE.PointLight(0xffffff, 0.5);
+            this.pointLight.position.set(3, 0, 0);
+            this.pointLightHelper = new THREE.PointLightHelper( this.pointLight, 0.1 );
+            this.scene.add( this.pointLightHelper );
+            this.scene.add(this.pointLight)
 			//make cube
 			//---------------------------------------------------
-			this.geometry = new THREE.BoxGeometry(1,1,1);
-			this.material = new THREE.MeshBasicMaterial( { color: 0x156091 } );
+			this.geometry = new THREE.BoxGeometry(2, 2, 2);
+			this.material = new THREE.MeshPhongMaterial( { color: 0xF3FFE2 } );
 			this.cube = new THREE.Mesh( this.geometry, this.material );
 			//---------------------------------------------------
 			//add cube to scene
 			this.scene.add( this.cube );
             
+            //this.renderer.render( this.scene, this.camera );
         },
 
         animate: function() {
             requestAnimationFrame( this.animate );
 
-			this.cube.rotation.x += 0.01;
+			
+            this.cube.rotation.x += 0.01;
 			this.cube.rotation.y += 0.01;
+            this.cube.rotation.z += 0.01;
+            
+            this.controls.update()
 
-			this.renderer.render( this.scene, this.camera );
+		    this.renderer.render( this.scene, this.camera );
         }
     },
 
