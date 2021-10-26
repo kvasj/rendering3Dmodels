484a72526102fcd33f9a4530d63e70830d9bebd9
Honza Kváš <kvasik98@seznam.cz>
2021-10-26 13:58:39 +0200

    add bootstrap navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,30 +1,73 @@
 <template>
     <div id="nav">
-        <router-link to="/threejs"><span>Three.js</span></router-link>
-        <router-link to="/babylonjs"><span>Babylon.js</span></router-link>
-        <router-link to="/model-viewer"><span>&ltmodel-viewer&gt</span></router-link>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Three.js
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link to="/threejs" class="dropdown-item"><span>Torus Knot Geometry</span></router-link></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Babylon.js
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link to="/babylonjs" class="dropdown-item"><span>in progress</span></router-link></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            &ltmodel-viewer&gt
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link to="/model-viewer" class="dropdown-item"><span>in progress</span></router-link></li>
+          </ul>
+        </li>
+
+      </ul>
     </div>
 </template>
 
 <style scoped>
 #nav {
-  padding: 30px;
+  margin: 20px;
   position: absolute;
   top: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  margin: 20px;
-  color: #2c3e50;
-  text-decoration: none;
+.navbar-nav{
+  flex-direction: unset;
 }
 
-#nav a:hover {
+.nav-item{
+  padding-right: 2rem;
+}
+
+.nav-link, .dropdown-item{
+  font-weight: bold;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+.nav-link:hover, .dropdown-item:hover{
   color: #42b983;
+  background-color: transparent;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.dropdown-menu{
+  background-color: transparent;
+  padding: 0px;
+  border: none;
+}
+
+.dropdown-item{
 }
 </style>
