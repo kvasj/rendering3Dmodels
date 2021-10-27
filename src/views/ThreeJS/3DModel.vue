ad8161f24eeb97af6a4510cfa956553d5acfe710
Honza Kváš <kvasik98@seznam.cz>
2021-10-27 16:10:19 +0200

    [THREE] add 3d models and render them

--- src/views/ThreeJS/3DModel.vue
+++ src/views/ThreeJS/3DModel.vue
@@ -5,23 +5,18 @@
 <script>
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader.js';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+//import { MTLLoader, OBJLoader } from 'three-obj-mtl-loader';
 
 export default {
     created () {
         window.addEventListener("resize", this.onWindowResize);
         return {
-            sphere: null,
             renderer: null,
             scene: null,
             camera: null,
-            ambientLight: null,
-            pointLight: null,
             controls: null,
-            objLoader: null,
-            mtlLoader: null,
         }
     },
     methods: {
@@ -30,8 +25,8 @@ export default {
 			//---------------------------------------------------
 			this.scene = new THREE.Scene();
             this.scene.background = new THREE.Color( 0x404040)
-			this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-			this.camera.position.z = 250;
+			this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
+			this.camera.position.z = 100;
             
             const canvas = document.querySelector('#canvas');
 			this.renderer = new THREE.WebGLRenderer({canvas});
@@ -45,25 +40,39 @@ export default {
 
             //make light
             //---------------------------------------------------
-            this.ambientLight = new THREE.AmbientLight(0x404040, 1);
-            this.scene.add(this.ambientLight);
-            /*
-            this.sphere = new THREE.SphereGeometry(1, 32, 16);
-            this.pointLight = new THREE.PointLight(0xff0040, 2, 50);
-			this.pointLight.add(new THREE.Mesh(this.sphere, new THREE.MeshBasicMaterial({ color: 0xff0000 })));
-			this.scene.add(this.pointLight);
-            */
+            const ambientLight = new THREE.AmbientLight(0x404040, 1);
+            this.scene.add(ambientLight);
+
+            const sphere = new THREE.SphereGeometry(0.2, 32, 16);
+
+            const far = 50;
+            const pointLight = new THREE.PointLight(0xffffff, 2, 100);
+			pointLight.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xffffff })));
+            pointLight.position.set(far,far,far)
+			this.scene.add(pointLight);
+            
+            const pointLight2 = new THREE.PointLight(0xffffff, 2, 100);
+			pointLight2.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xffffff })));
+            pointLight2.position.set(-far,far,far)
+			this.scene.add(pointLight2);
+
+            //axes
+            //---------------------------------------------------
+            const axesHelper = new THREE.AxesHelper( 50 );
+            this.scene.add( axesHelper );
             
             //make loader
             //---------------------------------------------------
+            const mtlLoader = new MTLLoader();
+            mtlLoader.load('/models/Trees/trees9.mtl', ( materials ) => {
+                materials.preload();
+                const objLoader = new OBJLoader();
+                objLoader.setMaterials(materials);
+                objLoader.load('/models/Trees/trees9.obj', ( object ) => {
+                    this.scene.add( object );
+                });
+            })
             
-            
-            this.objLoader = new OBJLoader();
-			this.objLoader.load('http://localhost:8080/public/models/FighterJet/10593_Fighter_Jet_SG_v1_iterations-2.obj', ( object ) => {
-                this.scene.add( object );
-                console.log(this.scene)
-                console.log(object)
-            });
             
             //this.renderer.render(this.scene, this.camera);
         },
